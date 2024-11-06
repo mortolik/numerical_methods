b8706e539cbd364db83f5a8f681e549a512b066a
Anatoliy Morozov <anatoly.morozov@radiogigabit.net>
2024-11-06 22:01:10 +0300

    Added Euler method. Create widget with plots

---
+++ EulerWidget.cpp
@@ -0,0 +1,3 @@
+#include "EulerWidget.hpp"
+
+EulerWidget::EulerWidget() {}
---
+++ EulerWidget.hpp
@@ -0,0 +1,10 @@
+#ifndef EULERWIDGET_HPP
+#define EULERWIDGET_HPP
+
+class EulerWidget
+{
+public:
+    EulerWidget();
+};
+
+#endif // EULERWIDGET_HPP
